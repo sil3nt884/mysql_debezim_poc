c62a09e4b3dadd76e4033e45c86ba9ef2116a638
sil3n <silentsoul884@outlook.com>
2023-12-14 14:29:40 +0000

    poc

---
+++ consumer.js
@@ -0,0 +1,35 @@
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:29092']
+});
+
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+const run = async () => {
+     try {
+         await consumer.connect();
+         await consumer.subscribe({topic: 'db.db.test', fromBeginning: false});
+
+         await consumer.run({
+             eachMessage: async ({topic, partition, message}) => {
+                 const {value} = message
+                 if (value) {
+                     const data = JSON.parse(message.value.toString())
+                     const {before, after} = data || {}
+                     if (before) {
+                         console.log('before', before)
+                     }
+                     if (after) {
+                         console.log('after', after)
+                     }
+                 }
+             },
+         });
+     } catch (e) {
+         console.error(e);
+     }
+};
+
+run().catch(console.error);
---
+++ test-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS db.test (
+	firstName VARCHAR(20),
+	secondName VARCHAR(20)
+
+
+)
